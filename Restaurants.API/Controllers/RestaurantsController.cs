b73d8419eb91855fb25e0a7a1ef077254af61b89
abdullrhman <3brhmaaan@gmail.com>
2024-12-01 14:41:43 +0200

    refactor the code to make swagger discover the status code of every endpoint

--- Restaurants.API/Controllers/RestaurantsController.cs
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -6,6 +6,7 @@ using Restaurants.Application.Restaurants.Commands.UpdateRestaurant;
 using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
 using Restaurants.Application.Restaurants.Queries.GetRestaurantById;
+using Restaurants.Domain.Entities;
 
 namespace Restaurants.API.Controllers;
 [Route("api/Restaurants")]
@@ -19,6 +20,7 @@ public class RestaurantsController : ControllerBase
     }
 
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK , Type = typeof(IEnumerable<RestaurantDto>))]
     public async Task<IActionResult> GetAll()
     {
         var restaurants = await _mediator.Send(new GetAllRestaurantsQuery());
@@ -27,6 +29,8 @@ public class RestaurantsController : ControllerBase
     }
 
     [HttpGet("{id}" , Name = "GetById")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK , Type = typeof(RestaurantDto))]
     public async Task<IActionResult> GetById(int id)
     {
         var restaurant = await _mediator.Send(new GetRestaurantByIdQuery() { Id = id});
@@ -38,6 +42,7 @@ public class RestaurantsController : ControllerBase
     }
 
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
     public async Task<IActionResult> CreateRestaurant(CreateRestaurantCommand command)
     {
         int id = await _mediator.Send(command);
@@ -46,6 +51,8 @@ public class RestaurantsController : ControllerBase
     }
 
     [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> DeleteRestaurant(int id)
     {
         var isDeleted = await _mediator.Send(new DeleteRestaurantCommand(id));
@@ -57,6 +64,8 @@ public class RestaurantsController : ControllerBase
     }
 
     [HttpPatch("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> UpdateRestaurant(int id , UpdateRestaurantCommand command)
     {
         command.Id = id;
