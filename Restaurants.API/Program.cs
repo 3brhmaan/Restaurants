dda0a103936838f9aea53e51a7b2b5b818ec4e23
abdullrhman <3brhmaaan@gmail.com>
2024-12-03 01:48:55 +0200

    refactor the code to make swagger allow Bearer Token header to be send with subsequent requests

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -5,13 +5,33 @@ using Serilog;
 using Serilog.Events;
 using Restaurants.API.Middlewares;
 using Restaurants.Domain.Entities;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("bearerAuth" , new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference{Type = ReferenceType.SecurityScheme , Id = "bearerAuth"}
+            } ,
+            []
+        }
+    });
+});
+
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
