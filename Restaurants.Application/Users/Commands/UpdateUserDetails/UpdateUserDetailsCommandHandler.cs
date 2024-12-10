352c1bbc2f4acfdff59e07b9ba29be3148c28234
abdullrhman <3brhmaaan@gmail.com>
2024-12-10 16:29:46 +0200

    transfaring UpdateUserDetails command and handler into a folder

--- Restaurants.Application/Users/Commands/UpdateUserDetails/UpdateUserDetailsCommandHandler.cs
+++ Restaurants.Application/Users/Commands/UpdateUserDetails/UpdateUserDetailsCommandHandler.cs
@@ -3,24 +3,24 @@ using Microsoft.AspNetCore.Identity;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Exceptions;
 
-namespace Restaurants.Application.Users.Commands;
+namespace Restaurants.Application.Users.Commands.UpdateUserDetails;
 public class UpdateUserDetailsCommandHandler
     : IRequestHandler<UpdateUserDetailsCommand>
 {
     private readonly IUserContext userContext;
     private readonly IUserStore<User> userStore;
 
-    public UpdateUserDetailsCommandHandler(IUserContext userContext , IUserStore<User> userStore)
+    public UpdateUserDetailsCommandHandler(IUserContext userContext, IUserStore<User> userStore)
     {
         this.userContext = userContext;
         this.userStore = userStore;
     }
 
-    public async Task Handle(UpdateUserDetailsCommand request , CancellationToken cancellationToken)
+    public async Task Handle(UpdateUserDetailsCommand request, CancellationToken cancellationToken)
     {
         var user = userContext.GetCurrentUser();
 
-        var dbUser = await userStore.FindByIdAsync(user.Id , cancellationToken);
+        var dbUser = await userStore.FindByIdAsync(user.Id, cancellationToken);
 
         if (dbUser is null)
             throw new NotFoundException($"User with Id: {user.Id} doesn't exist");
@@ -28,6 +28,6 @@ public class UpdateUserDetailsCommandHandler
         dbUser.DateOfBirth = request.DateOfBirth;
         dbUser.Nationality = request.Nationality;
 
-        await userStore.UpdateAsync(dbUser , cancellationToken);
+        await userStore.UpdateAsync(dbUser, cancellationToken);
     }
 }
